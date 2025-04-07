1fe68b948831bf4b179ecc9d155a450f0a9c71ff
nina031 <messaoudinina55@gmail.com>
2025-04-07 11:25:46 +0200

    intial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter } from "next/font/google";
+import { Lusitana as LusitanaFont } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const lusitana = LusitanaFont({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
